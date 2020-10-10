9ac2ea55b51516bfe0af7ce664d2a14fa1d10104
Virus629 <30345683+Virus629@users.noreply.github.com>
2020-10-10 20:33:41 +0300

    Replace console application using WinForms (UI)

--- Program.cs
+++ Program.cs
@@ -1,57 +1,45 @@
 ﻿using System;
-using Renci.SshNet;
+using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-using System.Globalization;
+using Renci.SshNet;
 
 /* 
  * Made for Fivem server called PelitupaNet
  * 
  * By: Eetu "Virus629" Lauren, 2020
- * 
- * 
- * TODO:
- * - Create Winforms UI
- * - Support to isVip
- * - Sanitize input string (only allow alphabets and numbers)
- * 
- * 
+ *
  * SOURCES: 
  * - https://stackoverflow.com/a/16168100
  * - https://en.it1352.com/article/d9f24e041aab4820983d94ae9534d800.html
 */
 
-namespace PelitupaWhitelister
+namespace PelitupaWhitelisterUI
 {
-    class Program
+    static class Program
     {
         // SSH Settings
-        private static string serverIp = "SSH_SERVER_IP"; // SSH Server ip
-        private static string sshUsername = "SSH_USER"; // SSH Username
-        private static string sshPasswd = "SSH_PASSWORD"; // SSH Password
+        private static readonly string sshServerIp = "SSH_SERVER_IP"; // SSH Server ip
+        private static readonly string sshUsername = "SSH_USER"; // SSH Username
+        private static readonly string sshPasswd = "SSH_PASSWORD"; // SSH Password
 
         // MySQL Settings
-        private static string dbServerIp = "DB_SERVER_IP"; // Database Ip
-        private static string dbUsername = "DB_USER"; // Database Username
-        private static string dbPasswd = "DB_PASSWORD"; // Database Password
-        private static string dbName = "DB_NAME"; // Database name
-        private static uint dbPort = 3306; // Database Port
+        private static readonly string dbServerIp = "DB_SERVER_IP"; // Database Ip
+        private static readonly string dbUsername = "DB_USER"; // Database Username
+        private static readonly string dbPasswd = "DB_PASSWORD"; // Database Password
+        private static readonly string dbName = "DB_NAME"; // Database name
+        private static readonly uint dbPort = 3306; // Database Port
 
+        [STAThread]
         public static void Main()
         {
-            Console.Write("Write player hexId: ");
-            string userInput = Console.ReadLine(); // Read user input
-            string playerHexId = userInput.ToLower(new CultureInfo("en-US", false)); // Convert string to lower cases
-
-            DatabaseConnection(playerHexId);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
 
-        protected static void DatabaseConnection(string hexId)
+        public static void Database(string hexId, bool isVip)
         {
-            SshClient client = null;
-            MySqlConnection conn = null;
-            MySqlCommand connCmd = null;
-
-            using (client = new SshClient(serverIp, sshUsername, sshPasswd))
+            using (SshClient client = new SshClient(sshServerIp, sshUsername, sshPasswd))
             {
                 client.Connect(); // Connect to ssh server
 
@@ -64,19 +52,20 @@ namespace PelitupaWhitelister
 
                     string connString = $"SERVER={dbServerIp};PORT={dbPort};UID={dbUsername};PASSWORD={dbPasswd};DATABASE={dbName}"; // MySQL connection string
 
-                    conn = new MySqlConnection(connString); // Create new mysql connection
+                    MySqlConnection conn = new MySqlConnection(connString); // Create new mysql connection
                     conn.Open(); // Open mysql connection
 
-                    connCmd = conn.CreateCommand();
+                    MySqlCommand connCmd = conn.CreateCommand();
 
-                    connCmd.CommandText = "INSERT INTO `whitelist`(steamId) VALUES (@steamId)"; // Mysql Query
-                    connCmd.Parameters.AddWithValue("@steamId", hexId); // Mysql query variables
+                    connCmd.CommandText = "INSERT INTO `whitelist`(steamId, isVip) VALUES (@steamId, @isVip)"; // Mysql Query
+                    connCmd.Parameters.AddWithValue("@steamId", hexId); // Mysql steamId variable
+                    connCmd.Parameters.AddWithValue("@isVip", isVip); // Mysql isVip variable
 
-                    Console.WriteLine("User " + hexId + " added to database!");  // Nice info to user
+                    // Console.WriteLine("User " + hexId + " added to database!");  // Nice info to user
                     connCmd.ExecuteNonQuery(); // Execute sql query
 
                     conn.Close(); // Close database / ssh connection
-                } 
+                }
                 else
                 {
                     Console.WriteLine("Cannot establish mysql connection!"); // Tell user if we cant establish connection to server
